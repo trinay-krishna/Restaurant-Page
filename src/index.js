c5ea996d658c0a422710752a003d15f18c4a38ce
trinay-krishna <trinaykrishna1@gmail.com>
2023-11-24 20:29:37 +0530

    Create Home Page with JS
    - Implemented home.js to dynamically generate the home page content.
    - Added event listeners for navigation buttons in index.js.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,39 @@
 import './styles.css';
-console.log("WORKING!");
+import setupHome from './home.js';
+
+const contentContainer=document.querySelector('#content');
+const headerElement=setupHome();
+
+const setupNavigation=(function(){
+
+    const homeBtn=document.querySelector('#Home');
+    const menuBtn=document.querySelector('#Menu');
+    const LocBtn=document.querySelector('#Loc');
+    setupEventListeners();
+
+    function setupEventListeners(){
+
+        homeBtn.addEventListener('click',
+            ()=>{
+                contentContainer.textContent="";
+                setupHome();
+            }
+        );
+
+        menuBtn.addEventListener('click',
+            ()=>{
+                contentContainer.textContent="";
+                setupMenu(headerElement);
+            }
+        );
+
+        LocBtn.addEventListener('click',
+            ()=>{
+                contentContainer.textContent="";
+                setupLocation(headerElement);
+            }
+        );
+    }
+
+
+});
